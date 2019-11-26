98a866185e5ef9c4cd9905a1e4d7386e1b37c6f0
Q485881 <sergey.mitchenko@bmw.de>
2019-11-26 18:15:38 +0100

    QEI (Encoder) module implemented

---
+++ qei.c
@@ -0,0 +1,82 @@
+/*
+ * File:   qei.c
+ * Author: Sergey
+ *
+ * Created on November 25, 2019, 2:46 PM
+ */
+
+#include <xc.h>
+#include "qei.h"
+#include <math.h>
+
+static long _longCNT = 0;
+
+static uint16_t _base_resolution;
+static uint16_t _gearing_ratio;
+static uint16_t _edge_gain;
+static uint16_t _wheel_circumference;
+
+static long _prev_count;
+
+// base_resolution = 16; gearing_ratio = 33; edge_gain = 4;
+void init_QEI(uint16_t base_resolution, uint16_t gearing_ratio, uint16_t edge_gain, uint16_t wheel_circumference) {
+
+    _base_resolution = base_resolution;
+    _gearing_ratio = gearing_ratio;
+    _edge_gain = edge_gain;
+    _wheel_circumference = wheel_circumference;
+
+     // Configure QEI pins as digital inputs
+    ADPCFGbits.PCFG5 = 1; // QEB on pin 7 shared with AN5
+    ADPCFGbits.PCFG4 = 1; // QEA on pin 6 shared with AN4
+
+    // Configure QEI module
+    QEICONbits.QEIM = 0;        // Disable QEI Module
+    QEICONbits.CNTERR = 0;      // Clear any count errors
+    QEICONbits.QEISIDL = 1;     // Discontinue operation when in idle mode/sleep
+    QEICONbits.SWPAB = 0;       // QEA and QEB not swapped
+    QEICONbits.PCDOUT = 0;      // Disable counter direction pin (normal I/O operation)
+    QEICONbits.POSRES = 0;      // index pulse does not reset POSCNT
+    DFLTCONbits.CEID = 1;       // Count error interrupts disabled
+    DFLTCONbits.QEOUT = 0;      // disable digital filters
+
+    QEICONbits.TQCS = 0;        // internal clock source (Tcy)
+    QEICONbits.QEIM = 0b111;    // x4 edge gain and reset POSCNT when == MAXCNT
+
+    // set initial counter value and maximum range
+    MAXCNT = 0xffff;    // set the highest possible time out
+    POSCNT = 0x7fff;    // set POSCNT into middle of range
+    _prev_count = POSCNT;
+
+    // Configure Interrupt controller
+    IFS2bits.QEIIF = 0;     // clear interrupt flag
+    IEC2bits.QEIIE = 1;     // enable QEI interrupt
+    IPC10bits.QEIIP = 5;    // set QEI interrupt priority
+}
+
+// interrupt service routine
+void __attribute__((interrupt, auto_psv)) _QEIInterrupt(void)
+{
+    IFS2bits.QEIIF = 0; // clear interrupt flag
+    if(POSCNT < 32768) {
+        _longCNT += 0x10000; // over-run condition caused interrupt
+    }
+    else {
+        _longCNT -= 0x10000; // under-run condition caused interrupt
+    }
+}
+
+float calc_velocity(uint16_t deltaTime) {
+    float f = 1.0f/(deltaTime);
+    long currentCount = _longCNT + POSCNT;
+    float v = (currentCount - _prev_count) * f
+    _prev_count = currentCount;
+    v = (2*M_PI / (base_resolution * gearing_ratio * edge_gain)) * v;
+    if (wheel_circumference == 0) {
+        return v;
+    }
+    else {
+        return wheel_circumference * v;
+    }
+}
+
---
+++ qei.h
@@ -0,0 +1,16 @@
+/*
+ * File:   qei.h
+ * Author: Sergey
+ *
+ * Created on November 25, 2019, 2:46 PM
+ */
+
+#ifndef QEI_H
+#define	QEI_H
+
+
+#include <xc.h>
+
+void init_QEI();
+
+#endif	/* PWM_H */
